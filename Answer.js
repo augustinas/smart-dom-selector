7f886b1c68f2d05fe32817cc5d1f2a3d7268b5e6
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-15 16:10:24 +0100

    Wrapped getElementById result in an Array for uniform behaviour

--- Answer.js
+++ Answer.js
@@ -12,7 +12,7 @@ var findElements = function (query) {
   if (query[0] === ".") {
     return document.getElementsByClassName(query.substr(1));
   } else if (query[0] === "#") {
-    return document.getElementById(query.substr(1));
+    return [document.getElementById(query.substr(1))];
   } else {
     return document.getElementsByTagName(query);
   }
