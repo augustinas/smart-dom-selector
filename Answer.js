ab9eb2aed4520a270248690204a291f849f06787
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-15 16:20:47 +0100

    Converted query results from HTMLCollection to Array

--- Answer.js
+++ Answer.js
@@ -9,11 +9,18 @@ var $ = function (selector) {
 };
 
 var findElements = function (query) {
+  var domElements;
   if (query[0] === ".") {
-    return document.getElementsByClassName(query.substr(1));
+    domElements = document.getElementsByClassName(query.substr(1));
   } else if (query[0] === "#") {
-    return [document.getElementById(query.substr(1))];
+    domElements = [document.getElementById(query.substr(1))];
   } else {
-    return document.getElementsByTagName(query);
+    domElements = document.getElementsByTagName(query);
   }
+
+  // Convert HTMLCollection to Array for easier processing
+  // Got the idea from following places:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
+  // http://stackoverflow.com/questions/222841/most-efficient-way-to-convert-an-htmlcollection-to-an-array
+  return [].slice.call(domElements);
 };
