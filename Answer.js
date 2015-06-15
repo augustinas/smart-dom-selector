c2d9b09aed4f33950063f2bcad3117063c907d65
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-15 16:02:40 +0100

    Implemented simple selector, only first component taken into account

--- Answer.js
+++ Answer.js
@@ -1,9 +1,19 @@
 var $ = function (selector) {
-  var elements = [];
+  var elements;
 
-  ////////////////////
-  // Your code here //
-  ////////////////////
+  selectorComponents = selector.split(/(?=[.#])/);
+
+  elements = findElements(selectorComponents.shift());
 
   return elements;
-}
+};
+
+var findElements = function (query) {
+  if (query[0] === ".") {
+    return document.getElementsByClassName(query.substr(1));
+  } else if (query[0] === "#") {
+    return document.getElementById(query.substr(1));
+  } else {
+    return document.getElementsByTagName(query);
+  }
+};
